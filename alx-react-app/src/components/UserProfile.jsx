23426bc70862a26a23dcf03c3d87656a8c0673c5
Rahma <150426318+Rahmaabdelrazeq@users.noreply.github.com>
2025-02-20 01:36:06 +0200

    Update UserProfile.jsx

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -4,8 +4,17 @@ const UserProfile = (props) => {
             <h2>{props.name}</h2>
             <p>Age: {props.age}</p>
             <p>Bio: {props.bio}</p>
+
+              <div style={{ border: '1px solid gray', padding: '10px', margin: '10px', borderRadius: '5px' }}>
+      <h2 style={{ color: 'blue', fontSize: '24px', marginBottom: '10px' }}>{props.name}</h2>
+      <p style={{ fontSize: '16px', margin: '5px 0' }}>
+        Age: <span style={{ fontWeight: 'bold', color: 'green' }}>{props.age}</span>
+      </p>
+      <p style={{ fontSize: '14px', color: 'gray' }}>Bio: {props.bio}</p>
+    </div>
         </div>
     );
 };
 
 export default UserProfile;
+
