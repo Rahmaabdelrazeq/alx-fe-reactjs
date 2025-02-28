fc97d623ba9b33c5c5c741b4f0a4bb61a347a64f
Rahma <150426318+Rahmaabdelrazeq@users.noreply.github.com>
2025-02-28 19:58:56 +0200

    Update Footer.jsx

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer style={{ backgroundColor: '#333', color: 'white', textAlign: 'center', padding: '10px', marginTop: '15px' }}>
-      <p style={{ margin: 0 }}>© 2025 My React App. All rights reserved.</p>
+    <footer style={{ backgroundColor: '#333', color: 'white', textAlign: 'center', padding: '10px' }}>
+      <p>© 2025 My React App. All rights reserved.</p>
     </footer>
   );
 }
