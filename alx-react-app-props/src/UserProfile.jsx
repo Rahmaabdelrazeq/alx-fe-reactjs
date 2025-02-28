20950d305141e9a8706039afd5782bb28091b516
Rahma <150426318+Rahmaabdelrazeq@users.noreply.github.com>
2025-02-28 21:46:39 +0200

    first commit55

--- alx-react-app-props/src/UserProfile.jsx
+++ alx-react-app-props/src/UserProfile.jsx
@@ -1,8 +1,7 @@
-// UserDetails.jsx
 import React, { useContext } from 'react';
 import UserContext from './UserContext';
 
-function UserDetails() {
+function UserProfile() {
   const userData = useContext(UserContext);
 
   return (
@@ -13,4 +12,4 @@ function UserDetails() {
   );
 }
 
-export default UserDetails;
+export default UserProfile;
