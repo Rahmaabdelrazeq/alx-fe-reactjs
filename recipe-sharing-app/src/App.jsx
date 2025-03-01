5a16e1fbd5faa75cee4b336a923e333390d97b5e
Rahma <150426318+Rahmaabdelrazeq@users.noreply.github.com>
2025-03-01 23:46:12 +0200

    new commits4

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -4,6 +4,8 @@ import AddRecipeForm from './components/AddRecipeForm'
 import DeleteRecipeButton from './components/DeleteRecipeButton'
 import FavoritesList from './components/FavoritesList'
 import EditRecipeForm from './components/EditRecipeForm'
+import SearchBar from './components/SearchBar'
+import RecommendationsList from './components/RecommendationsList'
 
 
 const App = () => {
@@ -15,6 +17,11 @@ const App = () => {
       <DeleteRecipeButton />
       <FavoritesList />
       <EditRecipeForm />
+     <SearchBar/>
+     <RecommendationsList/>
+    
+
+    
 
     </>
   )
--- recipe-sharing-app/src/components/SearchBar.jsx
+++ recipe-sharing-app/src/components/SearchBar.jsx
@@ -1,24 +1,11 @@
-import React, { useEffect } from 'react';
-import useRecipeStore from '../recipeStore';
+import React from 'react'
 
 const SearchBar = () => {
-  const setSearchTerm = useRecipeStore((state) => state.setSearchTerm);
-  const filterRecipes = useRecipeStore((state) => state.filterRecipes);
-  const searchTerm = useRecipeStore((state) => state.searchTerm);
-
-  useEffect(() => {
-    filterRecipes(); 
-  }, [searchTerm, filterRecipes]);
-
   return (
-    <input
-      type="text"
-      placeholder="Search recipes..."
-      value={searchTerm}
-      onChange={(e) => setSearchTerm(e.target.value)}
-      style={{ display: 'block', width: '100%', padding: '10px', marginBottom: '20px', fontSize: '16px' }}
-    />
-  );
-};
+    <div>
+        <h1>rahma</h1>
+    </div>
+  )
+}
 
-export default SearchBar;
+export default SearchBar
