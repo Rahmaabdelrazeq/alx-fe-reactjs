a31e82c62e493c549e29708f2390708d3a956741
Rahma <150426318+Rahmaabdelrazeq@users.noreply.github.com>
2025-03-02 00:28:39 +0200

    new commits4

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -4,19 +4,25 @@ import { v4 as uuidv4 } from 'uuid';
 export const useRecipeStore = create((set) => ({
     recipes: [],
 
-
+    // Add a new recipe
     addRecipe: (recipe) => set((state) => ({
         recipes: [...state.recipes, { id: uuidv4(), ...recipe }]
     })),
 
-
+    // Update an existing recipe
     updateRecipe: (id, updatedRecipe) => set((state) => ({
         recipes: state.recipes.map((recipe) =>
             recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe
         )
     })),
 
+    // Delete a recipe
     deleteRecipe: (id) => set((state) => ({
         recipes: state.recipes.filter((recipe) => recipe.id !== id)
+    })),
+
+    // Set the entire recipes array (new addition)
+    setRecipes: (newRecipes) => set(() => ({
+        recipes: newRecipes
     }))
 }));
