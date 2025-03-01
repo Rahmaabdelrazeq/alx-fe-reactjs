03b8002f36854935abf8c104bda97ec004d7fbe9
Rahma <150426318+Rahmaabdelrazeq@users.noreply.github.com>
2025-03-02 00:17:21 +0200

    new commits4

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,30 +1,27 @@
-import React from 'react'
-import RecipeDetails from './components/RecipeDetails'
-import AddRecipeForm from './components/AddRecipeForm'
-import DeleteRecipeButton from './components/DeleteRecipeButton'
-import FavoritesList from './components/FavoritesList'
-import EditRecipeForm from './components/EditRecipeForm'
-import SearchBar from './components/SearchBar'
-import RecommendationsList from './components/RecommendationsList'
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import RecipeDetails from './components/RecipeDetails';
+import AddRecipeForm from './components/AddRecipeForm';
+import DeleteRecipeButton from './components/DeleteRecipeButton';
+import FavoritesList from './components/FavoritesList';
+import EditRecipeForm from './components/EditRecipeForm';
+import SearchBar from './components/SearchBar';
+import RecommendationsList from './components/RecommendationsList';
 
 const App = () => {
   return (
-    <>
+    <Router>
+      <Routes>
+        <Route path="/RecipeDetails" element={<RecipeDetails />} />
+        <Route path="/add-recipe" element={<AddRecipeForm />} />
+        <Route path="/delete-recipe" element={<DeleteRecipeButton />} />
+        <Route path="/favorites" element={<FavoritesList />} />
+        <Route path="/edit-recipe" element={<EditRecipeForm />} />
+        <Route path="/search" element={<SearchBar />} />
+        <Route path="/recommendations" element={<RecommendationsList />} />
+      </Routes>
+    </Router>
+  );
+};
 
-      <RecipeDetails />
-      <AddRecipeForm />
-      <DeleteRecipeButton />
-      <FavoritesList />
-      <EditRecipeForm />
-     <SearchBar/>
-     <RecommendationsList/>
-    
-
-    
-
-    </>
-  )
-}
-
-export default App
+export default App;
