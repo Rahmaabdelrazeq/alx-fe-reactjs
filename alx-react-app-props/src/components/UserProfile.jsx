194d7bad2231eb0a6f4584ceff901b85b9d4d8b6
Rahma <150426318+Rahmaabdelrazeq@users.noreply.github.com>
2025-03-02 15:12:15 +0200

    new commits4

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,17 @@
-const UserProfile = (props) => {
+import { useContext } from "react";
+import UserContext from "./UserContext";
+
+function UserProfile() {
+  const userData = useContext(UserContext);
+
+  if (!userData) return <p>Loading...</p>;
+
   return (
-    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px', borderRadius: '8px', boxShadow: '2px 2px 10px rgba(0,0,0,0.1)' }}>
-      <h2 style={{ color: 'blue', marginBottom: '5px' }}>{props.name}</h2>
-      <p style={{ fontSize: '16px' }}>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
-      <p style={{ fontStyle: 'italic', color: 'darkgray' }}>Bio: {props.bio}</p>
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
     </div>
   );
-};
+}
 
 export default UserProfile;
