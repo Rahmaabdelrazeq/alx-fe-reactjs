9a3b44d3b3949e4703dd47414c574a056084d698
Rahma <150426318+Rahmaabdelrazeq@users.noreply.github.com>
2025-03-23 21:48:00 +0200

    new commit

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -2,6 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import RecipeDetail from "./components/RecipeDetail";
+
+function App() {
+  return (
+    <Router>
+      <div className="bg-gray-100 min-h-screen">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/recipe/:id" element={<RecipeDetail />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+
 
 function App() {
   const [count, setCount] = useState(0)
