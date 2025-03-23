eddaf020fa3c47cd1d79b5abad0cd7b06cc94864
Rahma <150426318+Rahmaabdelrazeq@users.noreply.github.com>
2025-03-23 21:38:20 +0200

    new commit

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -39,5 +39,18 @@ function App() {
   );
 }
 
+import HomePage from "./components/HomePage";
 
-export default App
+function App() {
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
